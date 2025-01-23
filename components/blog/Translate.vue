5846cd09bdb4f89a966ad6626a9e055913602d7f
zayn <1546492103@qq.com>
2025-01-24 02:28:33 +0800

    fix

--- components/blog/Translate.vue
+++ components/blog/Translate.vue
@@ -1,10 +1,10 @@
 <script setup>
 onMounted(() => {
-  translate.language.setLocal('chinese_simplified')
-  translate.service.use('client.edge')
-  translate.listener.start()
-  translate.execute()
   if (!import.meta.env.SSR) {
+    translate.language.setLocal('chinese_simplified')
+    translate.service.use('client.edge')
+    translate.listener.start()
+    translate.execute()
     const el = document.getElementById('translate')
     if (el) {
       const li = document.getElementById('transli')
